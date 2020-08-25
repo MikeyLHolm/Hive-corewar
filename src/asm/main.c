93adc4569e5cd733e98c447c8c8e63fb977cf336
Salah Adawi <sadawi@student.hive.fi>
2020-08-25 19:51:32 +0300

    labels are now converted to integers

--- src/asm/main.c
+++ src/asm/main.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 18:03:47 by sadawi            #+#    #+#             */
-/*   Updated: 2020/08/25 17:51:52 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/08/25 19:50:41 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -431,6 +431,64 @@ void	tokenize_file(t_asm *assm)
 	}
 }
 
+void	convert_argument_label(t_asm *assm, char **arg, int arg_position)
+{
+	t_token	*cur_token;
+	int		label_position;
+	char	*converted_arg;
+
+	label_position = 0;
+	cur_token = assm->token;
+	while (cur_token)
+	{
+		if (ft_strequ(cur_token->label, ft_strchr(*arg, ':') + 1))
+		{
+			if ((*arg)[0] == '%')
+				converted_arg = ft_strjoinfree(ft_strdup("%"), ft_itoa(label_position  - arg_position));
+			else
+				converted_arg = ft_itoa(label_position  - arg_position);
+			free(*arg);
+			*arg = converted_arg;
+			return ;
+		}
+		label_position += cur_token->size;
+		cur_token = cur_token->next;
+	}
+}
+
+void	convert_labels(t_asm *assm)
+{
+	t_token *cur_token;
+	int		position;
+
+	position = 0;
+	cur_token = assm->token;
+	while (cur_token)
+	{
+		if (cur_token->arg1 && ft_strchr(cur_token->arg1, ':'))
+			convert_argument_label(assm, &cur_token->arg1, position);
+		if (cur_token->arg2 && ft_strchr(cur_token->arg2, ':'))
+			convert_argument_label(assm, &cur_token->arg2, position);
+		if (cur_token->arg3 && ft_strchr(cur_token->arg3, ':'))
+			convert_argument_label(assm, &cur_token->arg3, position);
+		position += cur_token->size;
+		cur_token = cur_token->next;
+	}
+}
+
+void	print_tokens(t_token *token)
+{
+	t_token *cur;
+
+	cur = token;
+	while (cur)
+	{
+		ft_printf("CONVERTED TOKEN\n");
+		print_token_info(cur);
+		cur = cur->next;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	t_asm	*assm;
@@ -441,6 +499,8 @@ int		main(int argc, char **argv)
 	assm->file = read_file(argv[1]);
 	//check_file(assm->file);
 	tokenize_file(assm);
+	convert_labels(assm);
+	print_tokens(assm->token);
 	print_file(assm->file);
 	handle_writing(assm, argv[1]);
 	return (0);
