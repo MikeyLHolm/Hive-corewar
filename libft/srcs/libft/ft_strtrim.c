cf22d3aeb62e4c480aab83912c539439ed9d7d63
Salah Adawi <sadawi@student.hive.fi>
2020-08-26 18:24:28 +0300

    replace my faulty strtrim with mikko's version

--- libft/srcs/libft/ft_strtrim.c
+++ libft/srcs/libft/ft_strtrim.c
@@ -5,39 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/22 15:41:55 by sadawi            #+#    #+#             */
-/*   Updated: 2019/10/29 18:26:32 by sadawi           ###   ########.fr       */
+/*   Created: 2019/10/22 16:32:17 by mlindhol          #+#    #+#             */
+/*   Updated: 2020/08/26 18:20:10 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strtrim(char const *s)
 {
-	char	*str;
-	int		i;
-	int		j;
-	int		len;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len;
+	char			*str;
+	char			*new;
 
-	len = ft_strlen(s);
-	i = 0;
-	j = 0;
-	while (s[j] == ' ' || s[j] == '\t' || s[j] == '\n')
-		j++;
-	if (j == len)
-	{
-		if (!(str = (char*)malloc(1)))
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
-	while (s[len - i - 1] == ' ' || s[len - i - 1] == '\t'
-		|| s[len - i - 1] == '\n')
-		i++;
-	if (!(str = (char*)malloc(len - i - j + 1)))
+	if (!s)
 		return (NULL);
-	ft_strncpy(str, s + j, len);
-	str[len - i - j] = '\0';
-	return (str);
+	len = ft_strlen(s);
+	str = (char*)s;
+	while (len > 0 && (str[len - 1] == '\n' || str[len - 1] == ' ' ||
+				str[len - 1] == '\t'))
+		len--;
+	i = 0;
+	while (str[i] == '\n' || str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (len < i)
+		len = i;
+	if (!(new = (char *)ft_memalloc(sizeof(char) * (len - i + 1))))
+		return (NULL);
+	j = 0;
+	while (i < len)
+		new[j++] = str[i++];
+	new[j] = '\0';
+	return (new);
 }
