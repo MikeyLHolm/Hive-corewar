dceab98abe599eacdf9c68c0e8b31c605dcf70bc
Salah Adawi <sadawi@student.hive.fi>
2020-08-27 17:55:30 +0300

    save player id

--- src/vm/main.c
+++ src/vm/main.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/26 13:26:04 by mlindhol          #+#    #+#             */
-/*   Updated: 2020/08/27 17:50:12 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/08/27 17:55:07 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -158,10 +158,12 @@ void	get_player_info(t_player *player)
 t_player	*save_player(t_vm *vm, char *filename, char *n)
 {
 	t_player	*player;
+	static int	id;
 
 	(void)vm;
 	if (!(player = (t_player*)ft_memalloc(sizeof(t_player))))
 		handle_error("Malloc failed at save_player.");
+	player->id = id++;
 	player->filename = filename;
 	player->n = n ? ft_atoi(n) : 0;
 	get_player_info(player);
