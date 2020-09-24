7482cc3b7cf693f881f4ec8ebd4846504050adf9
Salah Adawi <sadawi@student.hive.fi>
2020-09-24 17:38:01 +0300

    add better versions of libft functions

--- libft/srcs/libft/ft_strsub.c
+++ libft/srcs/libft/ft_strsub.c
@@ -5,27 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/22 15:20:24 by sadawi            #+#    #+#             */
-/*   Updated: 2020/03/21 15:37:38 by sadawi           ###   ########.fr       */
+/*   Created: 2019/10/28 16:03:23 by elindber          #+#    #+#             */
+/*   Updated: 2020/09/23 13:48:06 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char	*str;
-	size_t	i;
+	char			*sub;
+	size_t			i;
+	unsigned int	index;
 
-	i = 0;
-	if (!(str = (char*)ft_memalloc(len + 1)))
+	if (!s)
 		return (NULL);
-	while (len)
+	sub = (char*)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	index = 0;
+	if (!sub || start > ft_strlen(s))
 	{
-		str[i++] = s[start++];
-		len--;
+		free(sub);
+		return (NULL);
 	}
-	str[i] = '\0';
-	return (str);
+	while (s[start] != '\0' && i < len)
+	{
+		sub[index] = s[start];
+		index++;
+		start++;
+		i++;
+	}
+	sub[index] = '\0';
+	return (sub);
 }
